42d40fde9f6e037ae42cbf7b4ad24d0895e55fba
Skyler Torian <skymanat7@yahoo.com>
2019-10-23 17:50:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,9 @@
 def using_include(array, element)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.include?("wow")
-  array.include?("Tardis")
+  array.include?(element)
+  array.include?(element)
 end
+using_include("wow","Tardis")
 
 def using_sort(array)
 array = ["wow","I","am","really","learning","arrays!"]
